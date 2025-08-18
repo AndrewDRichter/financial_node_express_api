b97c1cd4fcebb6c4b68d98c82f2d64f0055bee20
AndrewDRichter <andrewrichter1@hotmail.com>
2025-08-18 17:24:36 -0300

    ok

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,15 @@ import { Router, Request, Response } from "express";
 
 import { CreateUserController } from "./controllers/users/CreateUserController";
 import { DetailUserController } from "./controllers/users/DetailUserController";
+import { AuthUserController } from "./controllers/users/AuthUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 // User routes
 router.post('/users', new CreateUserController().handle)
-router.get('/users', new DetailUserController().handle)
+router.get('/users', isAuthenticated, new DetailUserController().handle)
+router.post('/login', new AuthUserController().handle)
 
 export { router };
